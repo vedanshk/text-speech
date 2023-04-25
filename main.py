50a650c609ba0daaea13a2ae222a8910842ebecc
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-04-25 11:18:25 +0530

    changed code

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ root.title('WizCode - TEXT_TO_SPEECH')
 
 ##heading
 Label(root, text = 'TEXT_TO_SPEECH' , font='arial 20 bold' , bg ='white smoke').pack()
-Label(root, text ='DataFlair' , font ='arial 15 bold', bg = 'white smoke').pack(side = BOTTOM)
+Label(root, text ='WizCoe' , font ='arial 15 bold', bg = 'white smoke').pack(side = BOTTOM)
 
 
 
@@ -40,8 +40,8 @@ entry_field.place(x=20 , y=100)
 def Text_to_speech():
     Message = entry_field.get()
     speech = gTTS(text = Message)
-    speech.save('DataFlair.mp3')
-    playsound('DataFlair.mp3')
+    speech.save('WizCode.mp3')
+    playsound('WizCode.mp3')
 
 def Exit():
     root.destroy()
